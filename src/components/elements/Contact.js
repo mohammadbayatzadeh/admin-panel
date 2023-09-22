fbbe028964eff70b32d42877f2c19f3727f4659d
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-09-22 17:04:48 +0330

    contact page designed

--- src/components/elements/Contact.js
+++ src/components/elements/Contact.js
@@ -1,41 +1,40 @@
 import React from "react";
 
-//styles
-import styles from "./Contact.module.css";
 
 //chart
 import WordChart from "./charts/WordChart";
 
 function Contact() {
   return (
-    <div className={styles.container}>
-      <div className={styles.subContainer}>
-        <div className={styles.left}>
+    <div class="w-full rounded-lg h-fit bg-text-color-secondary text-text-color-primary">
+      <div class="flex w-full lg:flex-row flex-col">
+        <div class="lg:w-5/12 w-full flex flex-col items-center justify-start bg-gradient-to-r from-bg-color-primary to-text-color-tertiary text-text-color-secondary lg:rounded-l-lg rounded-t-lg">
           <img
             src={require("../../data/users/profile.jpeg")}
             alt="mohammad bayatzadeh"
+            class="w-[200px] h-[200px] rounded-3xl mt-8 border-2 border-solid border-white"
           />
-          <p>Mohammad Bayatzadeh</p>
-          <p>Front-End developer</p>
+          <p class="mt-2.5">Mohammad Bayatzadeh</p>
+          <p class="mt-2.5">Front-End developer</p>
         </div>
-        <div className={styles.right}>
-          <p className={styles.head}>Information</p>
-          <div className={styles.part}>
-            <span className={styles.line}></span>
-            <p className={styles.head}>bio</p>
+        <div class="lg:w-7/12 w-full flex flex-col items-start justify-start py-4 px-8">
+          <p class="font-bold">Information</p>
+          <div class="mt-1">
+            <span class="flex w-full h-px bg-text-color-primary mt-2"></span>
+            <p class="font-bold">bio</p>
             <p>
               Self-studied Front-End developer with 2 year exprience with
               Reactjs, Nextjs frameworks.
             </p>
           </div>
-          <div className={styles.part}>
-            <p className={styles.head}>Contact Me</p>
-            <span className={styles.line}></span>
-            <p className={styles.head}>email</p>
+          <div class="mt-1">
+            <p class="font-bold">Contact Me</p>
+            <span class="flex w-full h-px bg-text-color-primary mt-2"></span>
+            <p class="font-bold">email</p>
             <p>mamadbayat777@gmail.com</p>
           </div>
-          <div className={styles.part}>
-            <p className={styles.head}>number</p>
+          <div class="mt-1">
+            <p class="font-bold">number</p>
             <p>+989368376614</p>
           </div>
         </div>
