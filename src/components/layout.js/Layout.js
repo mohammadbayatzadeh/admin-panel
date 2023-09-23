7863ec12ba647ae8947a6270eb38ce51ee9d938c
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-09-23 16:14:48 +0330

    header designed

--- src/components/layout.js/Layout.js
+++ src/components/layout.js/Layout.js
@@ -50,36 +50,55 @@ function Layout({ children }) {
   };
 
   return (
-    <div className={styles.main_container} id={[mode]}>
-      <header className={styles.header}>
-        <a href="/ " className={styles.logo}>
+    <div
+      className="fixed w-full bg-bg-color-secondary min-h-screen transition-all"
+      id={[mode]}
+    >
+      <header className="fixed flex items-center w-full bg-bg-color-primary px-2.5">
+        <a
+          href="/ "
+          className="no-underline text-3xl text-text-color-secondary mt-2.5 mb-5 ml-1 mr-auto"
+        >
           Admin Panel
         </a>
         <span onClick={() => themeHandler()}>
           {mode === "light" ? <Sun /> : <Moon />}
         </span>
-        <LayoutButton href="/contact">
-          <img src={require(`../../data/users/profile.jpeg`)} alt={"profile"} />
-        </LayoutButton>
-        <div className={styles.searchBox} onClick={() => searchHandler()}>
-          <LayoutButton>
+        <NavbarButton href="/contact">
+          <img
+            src={require(`../../data/users/profile.jpeg`)}
+            alt={"profile"}
+            className="w-full h-full rounded-lg"
+          />
+        </NavbarButton>
+        <div
+          className="h-[30px] w-fit flex border-2 border-solid border-text-color-secondary rounded-lg ml-1"
+          onClick={() => searchHandler()}
+        >
+          <a
+            className="flex justify-center items-center w-[30px] h-[30px] text-text-color-secondary rounded-lg ml-0 cursor-pointer "
+            href="#"
+          >
             <VscSearch />
-          </LayoutButton>
+          </a>
           <input
             ref={ref}
             onChange={(e) => (ref.current.value = e.target.value)}
+            className="w-0 border-none outline-none m-0 p-0 rounded-r-md transition-all text-black focus:p-1 focus:w-[100px]"
           />
         </div>
-        <LayoutButton>
-          <p className={styles.counter}>{Math.floor(Math.random() * 10)}</p>
+        <NavbarButton>
+          <p className="absolute translate-x-1.5 -translate-y-1 w-[12px] h-[12px] text-center bg-text-color-tertiary rounded-full text-[7px]">
+            {Math.floor(Math.random() * 10)}
+          </p>
           <VscBellDot />
-        </LayoutButton>
-        <LayoutButton>
+        </NavbarButton>
+        <NavbarButton>
           <VscSettingsGear />
-        </LayoutButton>
-        <LayoutButton>
+        </NavbarButton>
+        <NavbarButton>
           <VscSignOut />
-        </LayoutButton>
+        </NavbarButton>
       </header>
       <div className={styles.container}>
         <input type="checkbox" id="hamburger" name="hamburger" />
@@ -102,12 +121,19 @@ function Layout({ children }) {
 
 export default Layout;
 
-const LayoutButton = ({ href, children }) => {
+const NavbarButton = ({ href, children }) => {
   if (href)
     return (
-      <a className={styles.LayoutButton} href={href}>
+      <a
+        className="flex justify-center items-center w-[30px] h-[30px] text-text-color-secondary rounded-lg ml-1 cursor-pointer border-2 border-solid border-text-color-secondary"
+        href={href}
+      >
         {children}
       </a>
     );
-  return <div className={styles.LayoutButton}>{children}</div>;
+  return (
+    <div className="flex justify-center items-center w-[30px] h-[30px] text-text-color-secondary rounded-lg ml-1 cursor-pointer border-2 border-solid border-text-color-secondary">
+      {children}
+    </div>
+  );
 };
