2c9ab179977726a9eb46ea1c292a0cd04f39c151
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-11-24 15:41:47 +0330

    added filter

--- src/components/elements/Filter.js
+++ src/components/elements/Filter.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-function Filter() {
+function Filter({ setCategory }) {
+  const onChange = (e) => {
+    const { value } = e.target;
+    setCategory(value);
+  };
   return (
-    <select className="p-2 mb-2 mr-1 rounded-lg self-end">
+    <select className="p-2 mb-2 mr-1 rounded-lg self-end" onChange={onChange}>
       <option value="all">select category</option>
-      <option value="electronic">electronic</option>
-      <option value="jewerly">jewerly</option>
+      <option value="electronics">electronics</option>
+      <option value="jewelery">jewelery</option>
       <option value="men's clothing">men's clothing</option>
       <option value="women's clothing">women's clothing</option>
     </select>
--- src/components/templates/ManageItems.js
+++ src/components/templates/ManageItems.js
@@ -12,20 +12,32 @@ import Filter from "../elements/Filter";
 function ManageItems() {
   const dispatch = useDispatch();
   const { data, loading } = useSelector((state) => state.items);
-  const [category, setCategory] = useState({});
+  const [category, setCategory] = useState("all");
   const [filtered, setFiltered] = useState({});
 
   useEffect(() => {
     dispatch(getItems());
   }, []);
 
+  useEffect(() => {
+    let tempData = [...data];
+    tempData = tempData.filter((item) => {
+      if (category === "all") {
+        return item;
+      } else {
+        return item.category === category;
+      }
+    });
+    setFiltered(tempData);
+  }, [category]);
+  const allProducts = filtered.length ? filtered : data;
   if (loading) return <Loading />;
   return (
     <div className="w-full flex flex-col items-center">
-      <Filter />
+      <Filter setCategory={setCategory} />
       <div className="w-full mx-2 flex justify-between items-start flex-wrap">
         {data.length > 0 ? (
-          data.map((item) => <ItemCard key={item.id} {...item} />)
+          allProducts.map((item) => <ItemCard key={item.id} {...item} />)
         ) : (
           <p className="w-full bg-text-color-tertiary text-text-color-secondary text-center p-2 rounded-lg">
             There is some error in fetching data ...
