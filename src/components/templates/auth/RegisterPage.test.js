c52b5db36d56fb204d6f21b54dbf632c9cfc6958
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-12-08 17:33:50 +0330

    Update RegisterPage.test.js

--- src/components/templates/auth/RegisterPage.test.js
+++ src/components/templates/auth/RegisterPage.test.js
@@ -1,7 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
 import RegisterPage from "./RegisterPage";
 import "@testing-library/jest-dom";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent, act } from "@testing-library/react";
+import { helper } from "../../../utils/functions";
+import { ERR_MSG } from "../../../utils/constants";
 
 const getElement = (elm) => {
   const elms = {
@@ -28,4 +30,43 @@ describe("test inputs typing", () => {
     expect(getElement("email").value).toBe("");
     expect(getElement("password").value).toBe("");
   });
+
+  test("type in inputs", () => {
+    typeElement("salam", "email");
+    expect(getElement("email").value).toBe("salam");
+    typeElement("123456", "password");
+    expect(getElement("password").value).toBe("123456");
+  });
+
+  test("inEmpty calling check", () => {
+    const isEmpty = jest.spyOn(helper, "isEmpty");
+    helper.isEmpty(getElement("password").value);
+    helper.isEmpty(getElement("email").value);
+    expect(isEmpty).toHaveBeenCalledTimes(2);
+  });
 });
+
+describe("error handling check", () => {
+  beforeAll(() => {
+    expect(screen.queryByText(ERR_MSG.PASSWORD)).not.toBeInTheDocument();
+    expect(screen.queryByText(ERR_MSG.EMAIL)).not.toBeInTheDocument();
+  });
+  test("check email error handling", () => {
+    typeElement("salam", "email");
+    typeElement("1232", "password");
+    act(() => {
+      fireEvent.click(getElement("button"));
+    });
+    expect(screen.queryByText(ERR_MSG.EMAIL)).toBeInTheDocument();
+  });
+  test("password error handling", () => {
+    typeElement("salam@gmail.com", "email");
+    typeElement("1232", "password");
+    act(() => {
+      fireEvent.click(getElement("button"));
+    });
+    expect(screen.queryByText(ERR_MSG.PASSWORD)).toBeInTheDocument();
+  });
+});
+
+describe('navigation testing')
