880d0759eed2ad006fcbe6d15322d5057f37c19d
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-12-10 17:32:06 +0330

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,133 @@
-/** @type {import('jest').Config} */
-const config = {
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.tsx?$": "esbuild-jest",
-  },
-};
+module.exports = {
+  // automock: false,
 
-module.exports = config;
+  // bail: 0,
+
+  // cacheDirectory: "C:\\Users\\asus\\AppData\\Local\\Temp\\jest",
+
+  // clearMocks: true,
+  // collectCoverage: true,
+  // coverageDirectory: "coverage",
+  // moduleDirectories: ["node_modules"],
+  testEnvironment: "jest-environment-jsdom",
+  // transform: {
+  //   "\\.[jt]sx?$": "esbuild-jest",
+  // },
+  transformIgnorePatterns: ["node_modules/(?!(spacetime)/)"],
+
+  // collectCoverageFrom: undefined,
+
+  // coveragePathIgnorePatterns: [
+  //   "\\\\node_modules\\\\"
+  // ],
+
+  // coverageProvider: "babel",
+
+  // coverageReporters: [
+  //   "json",
+  //   "text",
+  //   "lcov",
+  //   "clover"
+  // ],
+
+  // coverageThreshold: undefined,
+
+  // dependencyExtractor: undefined,
+
+  // errorOnDeprecated: false,
+
+  // fakeTimers: {
+  //   "enableGlobally": false
+  // },
+
+  // forceCoverageMatch: [],
+
+  // globalSetup: undefined,
+
+  // globalTeardown: undefined,
+
+  // globals: {},
+
+  // maxWorkers: "50%",
+
+  // moduleFileExtensions: [
+  //   "js",
+  //   "mjs",
+  //   "cjs",
+  //   "jsx",
+  //   "ts",
+  //   "tsx",
+  //   "json",
+  //   "node"
+  // ],
+
+  // moduleNameMapper: {},
+
+  // modulePathIgnorePatterns: [],
+
+  // notify: false,
+
+  // notifyMode: "failure-change",
+
+  // preset: undefined,
+
+  // projects: undefined,
+
+  // reporters: undefined,
+
+  // resetMocks: false,
+
+  // resetModules: false,
+
+  // resolver: undefined,
+
+  // restoreMocks: false,
+
+  // rootDir: undefined,
+
+  // roots: [
+  //   "<rootDir>"
+  // ],
+
+  // runner: "jest-runner",
+
+  // setupFiles: [],
+
+  // setupFilesAfterEnv: [],
+
+  // slowTestThreshold: 5,
+
+  // snapshotSerializers: [],
+
+  // testEnvironmentOptions: {},
+
+  // testLocationInResults: false,
+
+  // testMatch: [
+  //   "**/__tests__/**/*.[jt]s?(x)",
+  //   "**/?(*.)+(spec|test).[tj]s?(x)"
+  // ],
+
+  // testPathIgnorePatterns: [
+  //   "\\\\node_modules\\\\"
+  // ],
+
+  // testRegex: [],
+
+  // testResultsProcessor: undefined,
+
+  // testRunner: "jest-circus/runner",
+
+  // transformIgnorePatterns: [
+  //   "\\\\node_modules\\\\",
+  //   "\\.pnp\\.[^\\\\]+$"
+  // ],
+
+  // unmockedModulePathPatterns: undefined,
+
+  // verbose: undefined,
+  // watchPathIgnorePatterns: [],
+
+  // Whether to use watchman for file crawling
+  // watchman: true,
+};
